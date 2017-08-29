0c457435272b4608c93380bf7e33fd71e5d4ddd5
Iveren Shaguy <iverenshaguy@gmail.com>
2017-08-29 09:15:49 +0100

    Fix test logic based on LFA review

--- tests/test.js
+++ tests/test.js
@@ -17,46 +17,32 @@ describe("Arithmetic/Geometric", function() {
 	});
 
 	describe("check for valid input", function() {
-		describe("array is empty", function() {
-			it("should return 0", function() {
-				assert.equal(myApp.aritGeo([]), 0);
-			});
+		it("should return 0", function() {
+			assert.equal(myApp.aritGeo([]), 0);
 		});
-	
-		describe("is neither arithmetic nor geometric", function() {
-			it('should return -1', function() {
-				assert.equal(myApp.aritGeo([-1, 2, -4, -13, -8]), -1);
-			});
+
+		it('should return -1', function() {
+			assert.equal(myApp.aritGeo([-1, 2, -4, -13, -8]), -1);
 		});
-	
-		describe("is arithmetic", function() {
-			it('should return "Arithmetic"', function() {
-				assert.equal(myApp.aritGeo([2, 4, 6, 8, 10]), "Arithmetic");
-			});
+
+		it('should return "Arithmetic"', function() {
+			assert.equal(myApp.aritGeo([2, 4, 6, 8, 10]), "Arithmetic");
 		});
-	
-		describe("is arithmetic", function() {
-			it('should return "Arithmetic"', function() {
-				assert.equal(myApp.aritGeo([1, 3, 5, 7, 9]), "Arithmetic");
-			});
+
+		it('should return "Arithmetic"', function() {
+			assert.equal(myApp.aritGeo([1, 3, 5, 7, 9]), "Arithmetic");
 		});
-	
-		describe("is arithmetic", function() {
-			it('should return "Arithmetic"', function() {
-				assert.equal(myApp.aritGeo([-1, -5, -9, -13, -17]), "Arithmetic");
-			});
+
+		it('should return "Arithmetic"', function() {
+			assert.equal(myApp.aritGeo([-1, -5, -9, -13, -17]), "Arithmetic");
 		});
-	
-		describe("is geometric", function() {
-			it('should return "Geometric"', function() {
-				assert.equal(myApp.aritGeo([1, 5, 25, 125, 625]), "Geometric");
-			});
+
+		it('should return "Geometric"', function() {
+			assert.equal(myApp.aritGeo([1, 5, 25, 125, 625]), "Geometric");
 		});
-	
-		describe("is geometric", function() {
-			it('should return "Geometric"', function() {
-				assert.equal(myApp.aritGeo([2, -4, 8, -16, 32, -64]), "Geometric");
-			});
+
+		it('should return "Geometric"', function() {
+			assert.equal(myApp.aritGeo([2, -4, 8, -16, 32, -64]), "Geometric");
 		});
 	});
 })
